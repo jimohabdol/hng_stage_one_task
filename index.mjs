f26df4239107a86dcfa654e7ee12e46de7475fa4
Abdulrahman Jimoh <jimoh_abdol@yahoo.com>
2023-09-08 01:08:18 +0100

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,34 @@
+import express from "express";
+import moment from 'moment';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const github_file_url = process.env.GITHUB_FILE_URL || 3000;
+const github_repo_url = process.env.GITHUB_REPO_URL || 3000;
+
+app.get('/api', (req, res) => {
+  const slack_name = req.query.slack_name || 'example_name';
+  const track = req.query.track || 'backend';
+  const currentDay = moment().format('dddd');
+  const utcTime = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
+
+  const response = {
+    slack_name: slack_name,
+    current_day: currentDay,
+    utc_time: utcTime,
+    track: track,
+    github_file_url: github_file_url,
+    github_repo_url: github_repo_url,
+    status_code: 200,
+  };
+
+  res.json(response);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
